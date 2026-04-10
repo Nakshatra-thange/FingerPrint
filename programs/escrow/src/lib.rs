6f9aad1f02e806e17ef0c756451127021503b495
Nakshatra-thange <nakshthange@gmail.com>
2026-04-10 23:03:24 +0530

    added new functions

--- programs/escrow/src/lib.rs
+++ programs/escrow/src/lib.rs
@@ -28,6 +28,98 @@ pub mod escrow{
             deadline > Clock::get()?.unix_timestamp,
             EscrowError::DeadlineInPast
         );
+
+        let esrcow = &mut ctx.accounts.escrow_account;
+        escrow.escrow_id= escrow_id;
+        escrow.payer = ctx.accounts.payer.key();
+        escrow.receiver = ctx.accounts.receiver.key();
+        escrow.event_description = event_description;
+        escrow.required_attestors = required_attestors;
+        escrow.threshold = threshold;
+        escrow.amount = amount;
+        escrow.deadline = deadline;
+        escrow.dispute_window_seconds = dispute_window_seconds;
+        escrow.bump = ctx.bumps.escrow_account;
+        escrow.status = EscrowStatus::Active;
+        escrow.threshold_met_at = None;
+        escrow.created_at = Clock::get()?.unix_timestamp;
+
+        let cpi_context = CpiContext::new(
+            ctx.accounts.system_program.to_account_info(),
+            anchor_lang::system_program::Transfer {
+                from: ctx.accounts.payer.to_account_info(),
+                to: ctx.accounts.escrow_vault.to_account_info(),
+
+            },
+        );
+        anchor_lang::system_program::transfer(cpi_context, amount)?;
+        emit!(EscrowCreated {
+            escrow_id,
+            payer: ctx.accounts.payer.key(),
+            receiver: ctx.accounts.receiver.key(),
+            amount,
+            threshold,
+            deadline,
+        });
+ 
+        Ok(())
+    }
+
+    pub fn mark_threshold_met(
+        ctx:Context<MarkThresholdMet>,
+        escrow_id:u64
+    )-> Result<()>{
+        let escrow = &mut ctx.accounts.escrow_account;
+        require!(
+            escrow.status == EscrowStatus::Active,
+            EscrowError::InvalidStatus
+        );
+
+        require!(
+            ctx.accounts.attestation_program.key() == crate::ATTESTATION_PROGRAM_ID,
+            EscrowError::Unauthorized
+        );
+
+        escrow.status = EscrowStatus::ThresholdMet;
+        escorw.threshold_met_at = Some(Clock::get()?.unix_timestamp);
+        emit!(ThresholdMet {
+            escrow_id,
+            timestamp: Clock::get()?.unix_timestamp,
+        });
+        Ok(())
+
+    }
+
+    pub fn release_funds(
+        ctx: Context<ReleaseFunds>, 
+        escrow_id: u64
+    )-> Result<()>{
+
+        let escrow = &mut ctx.accounts.escrow_account;
+        let clock = Clock::get()?;
+
+        require!(
+            escrow.status == EscrowStatus::ThresholdMet,
+            EscrowError::InvalidStatus
+        );
+
+        
+
+
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+
     }
 
 }
